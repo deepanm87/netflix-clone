b568e26a6825ee8da4010cc4fb51dc6aa7abc535
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-07-31 14:43:47 -0400

    added card component with hover styles

---
+++ components/card/card.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import Image from "next/image"
+import cls from "classnames"
+import { motion } from "framer-motion"
+import styles from "./card.module.css"
+
+const Card = ({ imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80", size = "medium" }) => {
+
+
+    const [imgSrc, setImgSrc] = useState(imgUrl)
+    const classMap = {
+        large: styles.lgItem,
+        medium: styles.medium,
+        small: styles.small
+    }
+
+    const handleOnError = () => {
+        setImgSrc("https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80")
+    }
+
+    return (
+        <div>
+            <div className={styles.container}>
+                Card
+                <motion.div
+                    className={cls(styles.imgMotionWrapper, classMap[size])}
+                    whileHover={{ scale: 1.2 }}
+                >
+                    <Image 
+                        src={imgSrc}
+                        alt="image"
+                        layout="fill"
+                        onError={handleOnError}
+                        className={styles.cardImg}
+                    />
+                </motion.div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import { Roboto_Slab } from "next/font/google";
 import Banner from "@/components/banner/banner";
 import NavBar from "@/components/nav/navbar"
+import Card from "@/components/card/card"
 
 const robotoSlab = Roboto_Slab({
   subsets: ["latin"],
@@ -17,6 +17,9 @@ export default function Home() {
         subTitle="a very cute dog"
         imgUrl="./static/clifford.webp"
       />
+      <Card imgUrl="/static/clifford.webp" size="large" />
+      <Card size="medium" />
+      <Card imgUrl="/static/clifford.webp" size="small" />
     </>
-  );
+  )
 }
