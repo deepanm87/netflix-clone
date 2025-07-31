7f1a527ff8dae9a38dfcb3f48cd49df58666d8c1
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-07-31 15:39:27 -0400

    added section card component with styles

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -4,7 +4,7 @@ import cls from "classnames"
 import { motion } from "framer-motion"
 import styles from "./card.module.css"
 
-const Card = ({ imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80", size = "medium" }) => {
+const Card = ({ imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80", size = "medium", id }) => {
 
 
     const [imgSrc, setImgSrc] = useState(imgUrl)
@@ -18,13 +18,14 @@ const Card = ({ imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644
         setImgSrc("https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80")
     }
 
+    const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 }
+
     return (
         <div>
             <div className={styles.container}>
-                Card
                 <motion.div
                     className={cls(styles.imgMotionWrapper, classMap[size])}
-                    whileHover={{ scale: 1.2 }}
+                    whileHover={{ ...scale }}
                 >
                     <Image 
                         src={imgSrc}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { Roboto_Slab } from "next/font/google";
 import Banner from "@/components/banner/banner";
 import NavBar from "@/components/nav/navbar"
-import Card from "@/components/card/card"
+import SectionCards from "@/components/card/section-cards"
+import styles from "@/styles/Home.module.css"
 
 const robotoSlab = Roboto_Slab({
   subsets: ["latin"],
@@ -9,17 +10,33 @@ const robotoSlab = Roboto_Slab({
 
 
 export default function Home() {
+
+  const disneyVideos = [
+    {
+      imgUrl: "/static/clifford.webp"
+    }, 
+      {
+        imgUrl: "/static/clifford.webp"
+    }, 
+    {
+      imgUrl: "/static/clifford.webp"
+    }
+  ]
+
+
   return (
-    <>
+    <div className={styles.container}>
       <NavBar username="deepanm" />
       <Banner 
         title="Clifford the red dog" 
         subTitle="a very cute dog"
         imgUrl="./static/clifford.webp"
       />
-      <Card imgUrl="/static/clifford.webp" size="large" />
-      <Card size="medium" />
-      <Card imgUrl="/static/clifford.webp" size="small" />
-    </>
+
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+        <SectionCards title="Disney" videos={disneyVideos} size="medium" />
+      </div>
+    </div>
   )
 }
