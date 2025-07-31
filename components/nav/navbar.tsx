b942099c4708f5d5d662a49267c7aeb4f78ec2f0
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-07-30 21:48:40 -0400

    added navbar with netflix logo and signout state

---
+++ components/nav/navbar.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react"
+import styles from "./navbar.module.css"
+import { useRouter } from "next/router"
+import Link from "next/link"
+import Image from "next/image"
+
+const Navbar = ( { username }) => {
+
+    const [showDropdown, setShowDropdown] = useState(false)
+    const router = useRouter()
+    
+    const handleOnClickHome = e => {
+        e.preventDefault()
+        router.push('/')
+    }
+
+    const handleOnClickMyList = e => {
+        e.preventDefault()
+        router.push('browse/my-list')
+    }
+
+    const handleShowDropdown = e => {
+        e.preventDefault()
+        setShowDropdown(!showDropdown)
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <Link className={styles.logoLink} href='/'>
+                    <div className={styles.logoWrapper}>
+                        <Image 
+                            src="/static/netflix.svg"
+                            alt="Netflix logo"
+                            width={128}
+                            height={34}
+                        />
+                    </div>
+                </Link>
+                <ul className={styles.navItems}>
+                    <li 
+                        className={styles.navItem}
+                        onClick={handleOnClickHome}
+                    >Home</li>
+                    <li 
+                        className={styles.navItem2}
+                        onClick={handleOnClickMyList}
+                        >My List</li>
+                </ul>
+                 
+                    <nav className={styles.navContainer}>
+                    <div>
+                        <button 
+                            className={styles.usernameBtn}
+                            onClick={handleShowDropdown}
+                            >
+                            <p className={styles.username}>{username}</p>
+                            <Image 
+                                src="/static/expand_more.svg"
+                                alt="expand more"
+                                width={24}
+                                height={24}
+                            />
+                        </button>
+                        { showDropdown && (
+                            <div className={styles.navDropdown}>
+                            <div>
+                                <Link href="/login" className={styles.linkName}>
+                                    Sign out
+                                </Link>
+                                
+                                <div className={styles.lineWrapper}></div>
+                            </div>
+                        </div>
+                        )}
+                    </div>  
+                </nav>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import { Roboto_Slab } from "next/font/google";
 import Banner from "@/components/banner/banner";
+import NavBar from "@/components/nav/navbar"
 
 const robotoSlab = Roboto_Slab({
   subsets: ["latin"],
 });
 
+
 export default function Home() {
   return (
     <>
-      <h1>Netflix</h1>
-
+      <NavBar username="deepanm" />
       <Banner 
         title="Clifford the red dog" 
         subTitle="a very cute dog"
